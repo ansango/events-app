7e2202438ab55b90e4bac9ff4ed600c9e26398c0
ansango <anibalsantosgo@gmail.com>
2021-09-22 21:02:51 +0200

    big calendar

--- client/src/components/calendar/CalendarScreen.js
+++ client/src/components/calendar/CalendarScreen.js
@@ -1,9 +1,28 @@
 import Navbar from "../ui/Navbar";
 
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment); // or globalizeLocalizer
+const myEventsList = [
+  {
+    start: moment().toDate(),
+    end: moment().add(1, "hours").toDate(),
+    title: "Event 1",
+  },
+];
+
 const CalendarScreen = () => {
   return (
     <div>
       <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+      />
     </div>
   );
 };
